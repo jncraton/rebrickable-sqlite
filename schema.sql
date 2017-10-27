6ac0aa3c29fbfe7f42f0166e3ef95166f048ec68
Jon Craton <jncraton@gmail.com>
2017-10-26 22:08:56 -0400

    Updated to refresh tables if database already exists

--- schema.sql
+++ schema.sql
@@ -1,19 +1,19 @@
 .mode csv
 
-create table colors (
+create table if not exists colors (
   id numeric primary key,
   name text,
   rgb text,
   is_trans text
 );
 
-create table parts (
+create table if not exists parts (
   part_num numeric primary key,
   name text,
   part_cat_id text
 );
 
-create table sets (
+create table if not exists sets (
   set_num numeric primary key,
   name text,
   year text,
@@ -21,7 +21,7 @@ create table sets (
   num_parts int
 );
 
-create table inventory_parts (
+create table if not exists inventory_parts (
   inventory_id numeric,
   part_num text,
   color_id numeric,
@@ -29,14 +29,22 @@ create table inventory_parts (
   is_spare int
 );
 
-create table inventory_sets (
+create table if not exists inventory_sets (
   inventory_id numeric,
   set_num text,
   quantity int
 );
+
+delete from colors;
+delete from parts;
+delete from sets;
+delete from inventory_parts;
+delete from inventory_sets;
 
 .import tables/colors.csv colors
 .import tables/parts.csv parts
 .import tables/sets.csv sets
 .import tables/inventory_parts.csv inventory_parts
 .import tables/inventory_sets.csv inventory_sets
+
+vacuum;
