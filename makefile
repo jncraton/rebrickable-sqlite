545ef6bebcae6b2ac120fad228c9a1af10fff31c
Jon Craton <jncraton@gmail.com>
2017-10-26 22:06:06 -0400

    Added more tables

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
-bricks.db: tables/colors.csv
+bricks.db: tables/colors.csv tables/parts.csv tables/sets.csv tables/inventory_parts.csv tables/inventory_sets.csv
 	sqlite3 bricks.db < schema.sql
 
-tables/%.csv: bricks.db
+tables/%.csv:
 	curl -o $@ https://m.rebrickable.com/media/downloads/$(subst tables/,,$@)
 
 clean:
--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+.mode csv
+
 create table colors (
   id numeric primary key,
   name text,
@@ -5,5 +7,36 @@ create table colors (
   is_trans text
 );
 
-.mode csv
+create table parts (
+  part_num numeric primary key,
+  name text,
+  part_cat_id text
+);
+
+create table sets (
+  set_num numeric primary key,
+  name text,
+  year text,
+  theme_id int,
+  num_parts int
+);
+
+create table inventory_parts (
+  inventory_id numeric,
+  part_num text,
+  color_id numeric,
+  quantity int,
+  is_spare int
+);
+
+create table inventory_sets (
+  inventory_id numeric,
+  set_num text,
+  quantity int
+);
+
 .import tables/colors.csv colors
+.import tables/parts.csv parts
+.import tables/sets.csv sets
+.import tables/inventory_parts.csv inventory_parts
+.import tables/inventory_sets.csv inventory_sets
