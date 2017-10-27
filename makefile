03b24a4f1f824a7e0752e3abf2f6475accb34f83
Jon Craton <jncraton@gmail.com>
2017-10-26 22:24:15 -0400

    Added the rest of the tables available for download

--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-bricks.db: tables/colors.csv tables/parts.csv tables/sets.csv tables/inventory_parts.csv tables/inventory_sets.csv
+bricks.db: tables/themes.csv tables/colors.csv tables/part_categories.csv tables/parts.csv tables/inventories.csv tables/sets.csv tables/inventory_parts.csv tables/inventory_sets.csv
 	sqlite3 bricks.db < schema.sql
 
 tables/%.csv:
--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 .mode csv
 
+create table if not exists themes (
+  id numeric primary key,
+  name text,
+  parent_id numeric
+);
+
 create table if not exists colors (
   id numeric primary key,
   name text,
@@ -7,10 +13,21 @@ create table if not exists colors (
   is_trans text
 );
 
+create table if not exists part_categories (
+  id numeric primary key,
+  name text
+);
+
 create table if not exists parts (
-  part_num numeric primary key,
+  part_num text primary key,
   name text,
   part_cat_id text
+);
+
+create table if not exists inventories (
+  id int primary key,
+  version int,
+  set_num text
 );
 
 create table if not exists sets (
@@ -35,14 +52,20 @@ create table if not exists inventory_sets (
   quantity int
 );
 
+delete from themes;
 delete from colors;
+delete from part_categories;
 delete from parts;
+delete from inventories;
 delete from sets;
 delete from inventory_parts;
 delete from inventory_sets;
 
+.import tables/themes.csv themes
 .import tables/colors.csv colors
+.import tables/part_categories.csv part_categories
 .import tables/parts.csv parts
+.import tables/inventories.csv inventories
 .import tables/sets.csv sets
 .import tables/inventory_parts.csv inventory_parts
 .import tables/inventory_sets.csv inventory_sets
