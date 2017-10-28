7e793b01aee2cd5b415e6c75c15c49e210aa4cf3
Jon Craton <jncraton@gmail.com>
2017-10-27 21:43:52 -0400

    Delete existing rows before import

--- src/rb_import/import.sql
+++ src/rb_import/import.sql
@@ -3,6 +3,15 @@
 
 pragma foreign_keys = 1;
 
+delete from inventory_sets;
+delete from inventory_parts;
+delete from inventories;
+delete from sets;
+delete from parts;
+delete from part_categories;
+delete from colors;
+delete from themes;
+
 .import tables/themes.csv themes
 .import tables/colors.csv colors
 .import tables/part_categories.csv part_categories
