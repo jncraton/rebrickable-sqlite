de15e0fe48e8627191a875b6afc0384131dde21a
Jon Craton <jncraton@gmail.com>
2018-06-15 10:45:28 -0400

    Added index on inventory_id

--- scripts/indices.sql
+++ scripts/indices.sql
@@ -6,6 +6,7 @@
 .timer on
 
 create index if not exists inventories_set_num_idx on inventories (set_num);
+create index if not exists inventory_parts_inventory_id_idx on inventory_parts (inventory_id);
 create index if not exists inventory_parts_part_num_idx on inventory_parts (part_num);
 create index if not exists inventory_parts_color_id_idx on inventory_parts (color_id);
 create index if not exists inventory_parts_part_num_color_id_idx on inventory_parts (part_num, color_id);
