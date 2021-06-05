9be05f83f67e8de161de10cb83c932c3781e2508
Jon Craton <jncraton@gmail.com>
2021-06-05 13:28:58 -0400

    Ignore various new parts

--- examples/new_parts.sql
+++ examples/new_parts.sql
@@ -1,6 +1,18 @@
-select part_num, name, num_sets, num_set_parts, year_from, part_url, part_img_url
+select part_num, parts.name, num_sets, num_set_parts, year_from, part_url, part_img_url
 from part_info
 natural join parts
 where year_from >= 2021 
   and part_num not like '%p%' -- skip printed parts
-  and name not like 'Sticker%' -- skip sticker sheets
+  and part_cat_id != 4 -- Duplo
+  and part_cat_id != 17 -- Non-LEGO (posters, etc)
+  and part_cat_id != 24 -- Other
+  and part_cat_id != 27 -- Minifig accessories
+  and part_cat_id != 28 -- Plants and Animals
+  and part_cat_id != 31 -- String, bands, and reels
+  and part_cat_id != 38 -- Flags, Signs, Plastic, Cloth
+  and part_cat_id != 56 -- Tools
+  and part_cat_id != 57 -- Duplo Figure
+  and part_cat_id != 58 -- Stickers
+  and part_cat_id != 59 -- Minifig heads
+  and part_cat_id != 60 -- Minifig upper body
+  and part_cat_id != 65 -- Minifig Headwear
